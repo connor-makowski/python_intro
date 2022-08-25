e0379a29e110da74fb953e2ac157ba9380c49e61
Connor Makowski <connorman528@gmail.com>
2022-08-25 11:50:02 -0400

    Add script to auto run all notebooks and use the script.

---
+++ run_notebooks.sh
@@ -0,0 +1,7 @@
+for file in ./*.ipynb
+do
+  echo $file
+  nbconvert_file=$(echo $file | sed 's/.ipynb/.nbconvert.ipynb/')
+  jupyter nbconvert --to notebook --allow-errors --execute $file
+  mv -f $nbconvert_file $file
+done
